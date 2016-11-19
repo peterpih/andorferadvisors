6dd867e21aa7bf5b08d68c731ac9526fbadd7d78
peterpih <railsnewbie257@gmail.com>
2016-11-19 12:54:03 -0600

    add blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,6 +12,8 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+   get '/blog' => 'blog#index', as: :blog
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
