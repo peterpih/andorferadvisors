856b5fcefbe44f34d4cb6ecf45f808572825bd92
peterpih <railsnewbie257@gmail.com>
2016-11-19 20:02:52 -0600

    add more functions for blog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,25 @@
 class BlogController < ApplicationController
   def index
+    logger.debug "-----BlogController(index)-----"
   end
-  
+
   def show
+    logger.debug "-----BlogController(show)-----"
+  end
+
+  def create
+    logger.debug "-----BlogController(create)-----"
+  end
+
+  def update
+    logger.debug "-----BlogController(update)-----"
+  end
+
+  def destroy
+    logger.debug "-----BlogController(destroy)-----"
+  end
+
+  def new
+    logger.debug "-----BlogController(new)-----"
   end
 end
