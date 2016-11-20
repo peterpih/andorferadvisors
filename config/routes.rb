3793bff961efdcd56492368ec1087019eeafee1f
peterpih <railsnewbie257@gmail.com>
2016-11-19 19:16:35 -0600

    added resources blog

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,14 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  get '/blog/index' => 'blog#index', as: :blog
-  get '/blog' => 'blog#index', as: :blog2
+  # get '/blog/index' => 'blog#index', as: :blog
+  # get '/blog' => 'blog#index', as: :blog2
   # get "/blog/index"
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :blog
 
   # Example resource route with options:
   #   resources :products do
